3c7ef7dba9a9b114d22ffd6217056871523be9c6
paarth <paarth@gmail.com>
2013-07-01 12:43:54 -0400

    second commit, traverses through files

--- main.py
+++ main.py
@@ -3,13 +3,29 @@
 import os
 import glob
 
-#load settings
-settingsfile = open('settings.conf','r')
-settingsdict = {}
-for line in settingsfile:
-	print(line)
-	tl = line.split('=')
-	settingsdict[ tl[0].strip() ] = tl[1].strip()
+def loadSettings (settingsfilename):
+	settingsfile = open(settingsfilename,'r')
+	settingsdict = {}
+	for line in settingsfile:
+		print(line)
+		tl = line.split('=')
+		settingsdict[ tl[0].strip() ] = tl[1].strip()
+	return settingsdict
+
+settingsdict = loadSettings ('settings.conf')
 
 print(settingsdict['basedir'])
 print(os.listdir(settingsdict['basedir']))
+
+
+for filetoadd in os.listdir(settingsdict['basedir']):
+	inputstring = 'raw'
+	os.system("start " + settingsdict['basedir'] + filetoadd)
+	while inputstring != 'next':
+		inputstring = input("command?")
+		if inputstring == 'tag':
+			print("okay...")
+			#add tag to image
+		
+
+print("Completed");
