ba700ff0144b824f693654c662df7abc67b78d5d
Paarth <Paarth@kiva.home>
2013-07-04 18:06:23 -0400

    cleaned up practice code

--- Bloom.py
+++ Bloom.py
@@ -2,24 +2,8 @@
 #Bloom beginnings
 import os
 import glob
-import BloomServer
-import BloomConfig
+#custom imports
+import server
+from organizers import Configuration
 
-SETTINGS_FILENAME = 'settings.conf'
-
-settingsdict = BloomConfig.loadSettings (SETTINGS_FILENAME)
-
-BloomServer.bloomGo(int(settingsdict['port']))
-print("Bloom Server Started")
-
-for filetoadd in os.listdir(settingsdict['basedir']):
-	inputstring = 'raw'
-	os.system("start " + settingsdict['basedir'] + filetoadd)
-	while inputstring != 'next':
-		inputstring = input("command?")
-		if inputstring == 'tag':
-			print("okay...")
-			#add tag to image
-
-
-print("Completed");
+server.start()
