7d9dc124be5adcfbde78755029114c2e0ce15e5f
Paarth <Paarth@kiva.home>
2013-07-05 03:51:45 -0400

    image thumbnails working (requires imagemagick), multi-file uploads now supported

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import web
 import sqlite3 as sql
+import os
 from organizers import Configuration     #Used to read JSON/XML configuration files.
 
 #setup for web.py
@@ -19,6 +20,8 @@ _urls = (
 	'/image/(.+)','image',
 	'/list/(.+)','search'
 )
+def make_thumbnail(filename):
+	os.system(r'convert static/archive/'+ filename +' -auto-orient -thumbnail 150x150 -unsharp 0x.5 static/archive/thumbnails/'+filename)
 
 class index:
 	def GET(self):
@@ -44,17 +47,33 @@ class upload:
 	def GET(self):
 		return _render.upload()
 	def POST(self):
-		x = web.input(userimage={})
-		filedir = 'static/archive' # change this to the directory you want to store the file in.
-		if 'userimage' in x: # to check if the file-object is created
-			filepath=x.userimage.filename.replace('\\','/')
+		filedir = 'static/archive' #directory to store the files in.
+		i = web.webapi.rawinput()
+		files = i.userimage
+		if not isinstance(files, list):
+			files = [files]
+		for f in files:
+			filepath=f.filename.replace('\\','/')
 			filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
 			fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
-			fout.write(x.userimage.file.read()) # writes the uploaded file to the newly created file.
+			fout.write(f.file.read()) # writes the uploaded file to the newly created file.
 			fout.close() # closes the file, upload complete.
 			something = _db.insert('images',file_path=filename)
-			print(something);
+			make_thumbnail(filename)
 		raise web.seeother('/images')
+
+		# x = web.input(userimage={})
+		# filedir = 'static/archive' # change this to the directory you want to store the file in.
+		# if 'userimage' in x: # to check if the file-object is created
+		# filepath=x.userimage.filename.replace('\\','/')
+		# filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+		# fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
+		# fout.write(x.userimage.file.read()) # writes the uploaded file to the newly created file.
+		# fout.close() # closes the file, upload complete.
+		# something = _db.insert('images',file_path=filename)
+		# make_thumbnail(filename)
+		# 	print(something)
+		# raise web.seeother('/images')
 
 class comingsoon:
 	def GET(self):
