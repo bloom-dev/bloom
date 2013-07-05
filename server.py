de6f0155640a2f0f22cc2b0480f959a490134782
Paarth <Paarth@kiva.home>
2013-07-04 21:47:46 -0400

    updated the web server quite abit

--- server.py
+++ server.py
@@ -7,11 +7,15 @@ from organizers import Configuration     #Used to read JSON/XML configuration fi
 #setup for web.py
 _config = Configuration.read("settings.json")
 _db = web.database(dbn='sqlite',db='bloom.db')
-_render = web.template.render('templates/')
+_render = web.template.render('templates/',base='wrapper')
+_render_naked = web.template.render('templates/')
 
 _urls = (
 	'/','index',
 	'/images','images',
+	'/tags','tags',
+	'/collections','comingsoon',
+	'/upload','comingsoon'
 )
 
 class index:
@@ -23,6 +27,15 @@ class images:
 	def GET(self):
 		image_list = _db.select('images')
 		return _render.images(image_list)
+
+class tags:
+	def GET(self):
+		tag_list = _db.select('tags')
+		return _render.tags(tag_list)
+
+class comingsoon:
+	def GET(self):
+		return _render.comingsoon()
 
 def start():
 	app = web.application(_urls,globals())
