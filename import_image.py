6370b9c3c0a44f15096155a30aac101f0fde2077
Oakland <ojp4@georgetown.edu>
2013-07-04 21:22:25 -0400

    Slight changes to import_image.py

--- import_image.py
+++ import_image.py
@@ -6,34 +6,16 @@ from organizers import Configuration     #Used to read JSON/XML configuration fi
 #[] Global Configuration Variables
 _config = Configuration.read("settings.json")
 
-def setup_image_database(cxn=None):
-    if cxn is None:
-        cxn = sql.connect(_config['image_database'])
-    
-    #[] Check if table exists
-    index_table = cxn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='{image_index_table}';".format(**_config))
-    
-    #[] Setup index table
-    if not index_table:
-        sql_create = '''CREATE TABLE {image_index_table}
-                        (id INTEGER NOT NULL AUTO_INCREMENT,
-                        file_path varchar(255),
-                        CONSTRAINT pk_id PRIMARY KEY (id)
-                        )'''.format(**_config)
-        cxn.execute(sql_create)
-    
-    return cxn
-
 def table_exists(cxn,table_name):
-    ret = cxn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='{0}';".format(table_name))
-    if (ret.fetchall()):   #if non-empty
+    results = cxn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='{0}';".format(table_name))
+    if (results.fetchall()):   #if non-empty
         return True
     else:
         return False
     
 def setup_default_tables(cxn=None):
     if cxn is None:
-        cxn = sql.connect()
+        cxn = sql.connect(_config['image_database'])
     
     
     #For each table
@@ -49,13 +31,7 @@ def setup_default_tables(cxn=None):
         cols = [" ".join([col_name,col_type,col_traits]) 
                        for col_name,col_type,col_traits 
                        in tinfo['columns']]
-        
-        #@deprecated: constraints not set up normally in SQLite
-        #Add Constraints
-#        constraints = [" ".join([const_name,const_type,const_targets])
-#                       for const_name,const_type,const_targets
-#                       in tinfo['constraints']]
-        
+
         columns_spec.extend(cols)
         #columns_spec.extend(constraints)
         spec_string = ",".join(columns_spec)
@@ -68,7 +44,7 @@ def setup_default_tables(cxn=None):
 #@incomplete    
 def import_image(image_path,cxn=None):
     if cxn is None:
-        cxn = setup_image_database()
+        cxn = sql.connect(_config['image_database'])
         
     #[] Insert Image
     sql_insert = '''INSERT INTO {0}
@@ -83,6 +59,8 @@ if __name__ == "__main__":
     
     #Establish SQL connection and build DB
     cxn = sql.connect(_config['bloom_db'])
+    
+    
     setup_default_tables(cxn)
 
     #Get files - via generator list comprehension    
